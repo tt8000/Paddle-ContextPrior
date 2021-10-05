f47fe72a8238451f2cdec1211c5807ed1c3c216f
Quan Tang <73922533+tt8000@users.noreply.github.com>
2021-10-05 21:04:52 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -6,11 +6,11 @@ import cv2
 
 
 class CPDataset(paddle.io.Dataset):
-    def __init__(self, data_txt, mode='train', transform=None):
+    def __init__(self, data_txt, mode='train', transforms=None):
         super(CPDataset, self).__init__()
         assert mode in ['train', 'val', 'test'], "mode is one of ['train', 'val', 'test']"
         self.mode = mode
-        self.transform = transform
+        self.transforms = transforms
         self.data = []
         with open(data_txt) as f:
             for line in f.readlines():
@@ -25,16 +25,16 @@ class CPDataset(paddle.io.Dataset):
             img = cv2.imread(self.data[idx][0]).astype('float32')
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             label = np.array(Image.open(self.data[idx][1]))
-            if self.transform:
-                img, label = self.transform(img, label)              
+            if self.transforms:
+                img, label = self.transforms(img, label)              
             label = label - 1
             label[label == -1] = 255
             return img, label.astype('int64')
         else:
             img = cv2.imread(self.data[idx]).astype('float32')
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            if self.transform:
-                img = self.transform(img)  
+            if self.transforms:
+                img = self.transforms(img)  
             return img
 
     def __len__(self):
